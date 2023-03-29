380f8ba0bf87bbb785c2a5f04fce632711e5c8fd
elbarbarihanane <hananeelbarbari2000@gmail.com>
2023-03-29 22:11:19 +0000

    Partie 1 de la 2emeactivite jpa

---
+++ src/test/java/ma/emsi/jpahospital52/JpaHospital52ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.emsi.jpahospital52;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHospital52ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
